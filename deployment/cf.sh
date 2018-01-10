daf463fc3fd13de4bcb1891dc6ba4f80f53fc4b5
Christoph Huber <huber.chrigu@gmail.com>
2018-01-10 17:20:44 +0100

    Initial demo app

---
+++ deployment/cf.sh
@@ -0,0 +1,12 @@
+cf login -a https://api.lyra-836.appcloud.swisscom.com -u huber.chrigu@gmail.com
+cf push paas-demo -p target/paas-demo-0.0.1-SNAPSHOT.jar
+
+cf create-service mongo mongodb small
+cf create-service rabbitmqent usage rabbitmq
+cf create-service mariadbent usage maria
+
+cf bind-service paas-demo mongo
+cf bind-service paas-demo rabbitmq
+cf bind-service paas-demo maria
+
+cf restart paas-demo
---
+++ deployment/oc.sh
@@ -0,0 +1,1 @@
+oc login https://api.starter-ca-central-1.openshift.com -u huber.chrigu@gmail.com
