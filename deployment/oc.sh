b3e5df00942d4a3b410553fb430f517c28da3193
Christoph Huber <huber.chrigu@gmail.com>
2018-01-11 16:24:50 +0100

    Profile for openshift

--- deployment/oc.sh
+++ deployment/oc.sh
@@ -1,1 +1,11 @@
 oc login https://api.starter-ca-central-1.openshift.com -u huber.chrigu@gmail.com
+
+oc new-build redhat-openjdk18-openshift:latest~git@bitbucket.org:huberchrigu/paas-demo.git --build-secret=ssh --source-secret=ssh
+oc new-app paas-demo-new
+oc expose svc/paas-demo-new
+
+oc new-app luiscoms/openshift-rabbitmq:management
+
+oc new-app mongodb-persistent
+
+oc new-app mysql-persistent
