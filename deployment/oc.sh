c387f1d60d27f42ac152a5c71217e2a2850df464
Christoph Huber <huber.chrigu@gmail.com>
2018-01-15 10:46:24 +0100

    Describe the manual adaption of the environment variables

--- deployment/oc.sh
+++ deployment/oc.sh
@@ -9,3 +9,52 @@ oc new-app luiscoms/openshift-rabbitmq:management
 oc new-app mongodb-persistent
 
 oc new-app mysql-persistent
+
+echo "Adapt Deployment YAML:"
+echo "...
+spec:
+      containers:
+        - env:
+            - name: SPRING_PROFILES_ACTIVE
+              value: openshift
+            - name: OPENSHIFT_MYSQL_DB_USERNAME
+              valueFrom:
+                secretKeyRef:
+                  key: database-user
+                  name: mysql
+            - name: OPENSHIFT_MYSQL_DB_PASSWORD
+              valueFrom:
+                secretKeyRef:
+                  key: database-password
+                  name: mysql
+            - name: OPENSHIFT_MYSQL_DB_HOST
+              value: 172.30.43.218
+            - name: OPENSHIFT_MYSQL_DB_PORT
+              value: '3306'
+            - name: OPENSHIFT_MYSQL_DB_NAME
+              valueFrom:
+                secretKeyRef:
+                  key: database-name
+                  name: mysql
+            - name: OPENSHIFT_MONGODB_DB_USERNAME
+              valueFrom:
+                secretKeyRef:
+                  key: database-user
+                  name: mongodb
+            - name: OPENSHIFT_MONGODB_DB_PASSWORD
+              valueFrom:
+                secretKeyRef:
+                  key: database-password
+                  name: mongodb
+            - name: OPENSHIFT_MONGODB_DB_NAME
+              valueFrom:
+                secretKeyRef:
+                  key: database-name
+                  name: mongodb
+            - name: OPENSHIFT_MONGODB_DB_HOST
+              value: 172.30.210.250
+            - name: OPENSHIFT_MONGODB_DB_PORT
+              value: '27017'
+..."
+
+echo "Or use client tools alternatively"
