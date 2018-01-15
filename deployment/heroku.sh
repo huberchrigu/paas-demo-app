cd508c2603df281baf112be3f28c7768a49f2651
Christoph Huber <huber.chrigu@gmail.com>
2018-01-15 11:40:35 +0100

    Fixed Procfile to pass port

--- deployment/heroku.sh
+++ deployment/heroku.sh
@@ -1,1 +1,9 @@
 heroku login
+heroku create sr-paas-demo
+git push heroku master
+
+heroku addons:create cleardb:ignite
+heroku addons:create mongolab:sandbox
+heroku addons:plans rabbitmq-bigwig
+
+heroku app:restart
